6c0299e6dc194e9cc0f22da9f9011eada51f1748
Gabriel Sroka <gabrielsroka@gmail.com>
2022-02-01 17:33:36 -0800

    Update hn_async.py

--- hn_async.py
+++ hn_async.py
@@ -6,10 +6,8 @@ url = 'https://hacker-news.firebaseio.com'
 
 async def main():
     async with aiohttp.ClientSession(url) as session:
-        async with session.get('/v0/topstories.json') as r:
-            ids = await r.json()
-
-        tasks = [task(session, f'/v0/item/{id}.json') for id in ids[:total]]
+        ids = await fetch(session, '/v0/topstories.json')
+        tasks = [fetch(session, f'/v0/item/{id}.json') for id in ids[:total]]
         items = await asyncio.gather(*tasks)
     
     for i, item in enumerate(items, start=1):
@@ -17,7 +15,7 @@ async def main():
             print(f"{i:2}) {item['title']} | {item.get('url')}")
             print(f"    https://news.ycombinator.com/item?id={item['id']} | {item['by']} | {item['score']}")
 
-async def task(session, url):
+async def fetch(session, url):
     async with session.get(url) as r:
         return await r.json()
 
